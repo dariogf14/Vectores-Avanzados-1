be735251388026e6c5ca353c51ca1cb668796fc8
Mañana <Maana@DESKTOP-T6B1QGE>
2024-02-01 11:35:33 +0100

    Creada la operación para añadir valores a las asignaturas
    Añadidos getters setters y un toString a Asignatura

--- src/vectores_avanzados_1/Alumno.java
+++ src/vectores_avanzados_1/Alumno.java
@@ -7,12 +7,21 @@ public class Alumno {
 	private String apellidos;
 	private String telefono;
 	private Asignatura asignaturas[];
+	private int i = 0;
 	
-	public Alumno(String Nombre, String Apellidos, String Telefono) {
-		this.nombre=Nombre;
-		this.apellidos=Apellidos;
-		this.telefono=Telefono;
+	public Alumno(String nombre, String apellidos, String telefono) {
+		this.nombre=nombre;
+		this.apellidos=apellidos;
+		this.telefono=telefono;
 		asignaturas = new Asignatura[4];
+	}
+	
+	public void addAsignatura(String nombre, int nota1, int nota2, int notafinal) {
+		asignaturas[i].setNombre(nombre);
+		asignaturas[i].setNota1eva(nota1);
+		asignaturas[i].setNota2eva(nota2);
+		asignaturas[i].setNotaFinal(notafinal);
+		i++;
 	}
 	
 }
--- src/vectores_avanzados_1/Asignatura.java
+++ src/vectores_avanzados_1/Asignatura.java
@@ -8,4 +8,38 @@ public class Asignatura {
 	private int nota2eva;
 	private int notaFinal;
 	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getNota1eva() {
+		return nota1eva;
+	}
+	public void setNota1eva(int nota1eva) {
+		this.nota1eva = nota1eva;
+	}
+	public int getNota2eva() {
+		return nota2eva;
+	}
+	public void setNota2eva(int nota2eva) {
+		this.nota2eva = nota2eva;
+	}
+	public int getNotaFinal() {
+		return notaFinal;
+	}
+	public void setNotaFinal(int notaFinal) {
+		this.notaFinal = notaFinal;
+	}
+	@Override
+	public String toString() {
+		return "Asignatura [nombre=" + nombre + ", nota1eva=" + nota1eva + ", nota2eva=" + nota2eva + ", notaFinal="
+				+ notaFinal + "]";
+	}
+	
+	
+	
+	
 }
