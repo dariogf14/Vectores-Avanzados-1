eb1bfa13afdb6dcf42d387c925592bda037b3918
Mañana <Maana@DESKTOP-T6B1QGE>
2024-02-01 11:21:23 +0100

    Añadida la clase aula y creados los constructores

--- src/vectores_avanzados_1/Alumno.java
+++ src/vectores_avanzados_1/Alumno.java
@@ -6,6 +6,13 @@ public class Alumno {
 	private String nombre;
 	private String apellidos;
 	private String telefono;
-	private int calificaciones[];
+	private Asignatura asignaturas[];
+	
+	public Alumno(String Nombre, String Apellidos, String Telefono) {
+		this.nombre=Nombre;
+		this.apellidos=Apellidos;
+		this.telefono=Telefono;
+		asignaturas = new Asignatura[4];
+	}
 	
 }
--- src/vectores_avanzados_1/Aula.java
+++ src/vectores_avanzados_1/Aula.java
@@ -2,4 +2,12 @@ package vectores_avanzados_1;
 
 public class Aula {
 
+	private int numeroAula;
+	private Alumno alumnos[];
+	
+	public Aula(int numero) {
+		this.numeroAula=numero;
+		alumnos = new Alumno[5];
+	}
+	
 }
